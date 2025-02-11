03e6813ecb0470a702b57c5c44dd7cf0e8e03e16
eldadstar <eldadfikre456@gmail.com>
2025-02-11 17:56:54 +0300

    Make Design Rotate

--- frontend/src/components/CreateComponent.jsx
+++ frontend/src/components/CreateComponent.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FaTrashAlt } from 'react-icons/fa';
+import Element from './Element';
 
 const CreateComponenet = ({current_component,info,removeComponent}) => {
     console.log(info)
@@ -39,8 +40,75 @@ const CreateComponenet = ({current_component,info,removeComponent}) => {
         }}
         className='absolute group hover:border-[2px] hover:border-indigo-500'
         >
+            <Element id={randValue} info={info} exId = "" />
         {
             current_component.id === info.id && <div onClick={() => removeComponent(info.id)} className='px-3 py-2 bg-white absolute top-0 right-0 hidden group-hover:block cursor-pointer rounded-md'>
+                <FaTrashAlt/>
+            </div>
+        }
+        </div>
+    }
+
+    if(info.name === 'shape' && info.type === 'circle'){
+        html = <div id={randValue} onClick={()=> info.setCurrentComponent(info)} 
+        style={{
+            left: info.left + 'px',
+            top: info.top + 'px',
+            zIndex: info.z_index,
+            transform: info.rotate ? `rotate(${info.rotate}deg)`: 'rotate(0deg)'
+        }}
+        className='absolute group hover:border-[2px]
+      hover:border-indigo-500'
+        >
+            <div id={`${randValue}c`} className='rounded-full' 
+            style={{
+            width: info.width + 'px',
+            height: info.height + 'px',
+            background: info.color,
+            opacity: info.opacity,
+            }}>
+
+            </div>
+            <Element id={randValue} info={info} exId = {`${randValue}c`} />
+        {
+            current_component.id === info.id && <div onClick={() => 
+            removeComponent(info.id)} className='px-3 py-2 bg-white 
+            absolute top-0 right-0 hidden group-hover:block cursor-pointer 
+            rounded-md'>
+                <FaTrashAlt/>
+            </div>
+        }
+        </div>
+    }
+
+
+    if(info.name === 'shape' && info.type === 'triangle'){
+        html = <div id={randValue} onClick={()=> info.setCurrentComponent(info)} 
+        style={{
+            left: info.left + 'px',
+            top: info.top + 'px',
+            zIndex: info.z_index,
+            transform: info.rotate ? `rotate(${info.rotate}deg)`: 'rotate(0deg)'
+        }}
+        className='absolute group hover:border-[2px]
+      hover:border-indigo-500'
+        >
+            <div id={`${randValue}t`} className='rounded-full' 
+            style={{
+            width: info.width + 'px',
+            height: info.height + 'px',
+            background: info.color,
+            opacity: info.opacity,
+            clipPath: 'polygon(50% 0,100% 100%,0 100%)'
+            }}>
+
+            </div>
+            <Element id={randValue} info={info} exId = {`${randValue}t`} />
+        {
+            current_component.id === info.id && <div onClick={() => 
+            removeComponent(info.id)} className='px-3 py-2 bg-white 
+            absolute top-0 right-0 hidden group-hover:block cursor-pointer 
+            rounded-md'>
                 <FaTrashAlt/>
             </div>
         }
